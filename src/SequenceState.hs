d7655c3ec9e3cc6f4324cb75d8b148c25401618a
Johan Brinch <zerrez@gmail.com>
2009-04-24 23:26:29 +0200

    SequenceState

--- src/SequenceState.hs
+++ src/SequenceState.hs
@@ -11,7 +11,7 @@ c_MAX_SIZE = 2**32-1
 
 _find f n EmptyL = Nothing
 _find f n (x :< xs) =
-    if f x then Just n
+    if f x then Just (n, x)
     else _find f (n+1) (viewl xs)
 
 find seq f = _find f 0 (viewl seq)
@@ -19,62 +19,52 @@ find seq f = _find f 0 (viewl seq)
 
 instance State (Word, Seq (Word, (Seq Word))) where
 
-    empty = (1, fromList [])
+    empty = (0, fromList [])
 
     lookupE s arr off =
-        let (next, env) = s in 
-        case find env (\x -> fst x == arr) of
-          Nothing -> Nothing
-          Just index' -> 
-              let (idx, arr') = index env (fromIntegral index') in
-              Just (index arr' (fromIntegral off))
+        let (nidx, env) = s in 
+        do {
+          (index', (idx, arr')) <- find env (\x -> fst x == arr) ; 
+          return (index arr' (fromIntegral off))
+        }
 
     updateE s arr off val = 
-        let (next, env) = s in 
+        let (nidx, env) = s in 
         case find env (\x -> fst x == arr) of 
           Nothing -> Nothing
-          Just index' -> 
-              let (idx, arr') = index env (fromIntegral index') in 
+          Just (index', (idx, arr')) -> 
               let arr'' = update (fromIntegral off) val arr'  in 
               let env'  = update index' (idx, arr'') env in 
-              Just (next, env')
-                  
-{-        
-
-    update s arr off val = 
-        let (nextidx, env) = s in 
-        Just (nextidx, ((arr, off, val) : filter (\(a,b,_) -> a /= arr || b /= off) env))
-
+              Just (nidx, env')
 
     allocate s cap = 
-        let (idx, env) = s in 
-        let nextidx = idx + 1 in 
-        Just ((nextidx, ([(idx, i, 0) | i <- [0..cap]] ++ env)), idx)
+        let (nidx, env) = s in 
+        let seq = fromList [ 0 | x <- [0..cap] ] in
+        let env' = (nidx, seq) <| env in
+        Just ((nidx+1, env'), nidx)
 
-
-    swap s arr0 arr1 =
-         let (nextidx, env) = s in 
-         let env' = map (\(arr, off, val) -> 
-                            if arr == arr0 then
-                                (arr1, off, val)
-                            else if arr == arr1 then
-                                     (arr0, off, val)
-                                 else (arr, off, val)) env
-         in Just (nextidx, env')
-
+    swap s arr0 arr1 = 
+        let (nidx, env) = s in 
+        do {
+          (idx0, arr0') <- find env (\x -> fst x == arr0) ; 
+          (idx1, arr1') <- find env (\x -> fst x == arr1) ; 
+          return (nidx, update idx1 arr0' (update idx0 arr1' env))
+        }
 
     copy s from to = 
-        let (nextidx, env) = s in 
-        let env' = map (\(_,off,val) -> (to, off, val)) 
-                  (filter (\(arr,_,_) -> arr == from) env) ++ 
-                  (filter (\(arr,_,_) -> arr /= to) env) 
-        in Just (nextidx, env')
-
+        let (nidx, env) = s in 
+        do {
+          (idx0, arr0) <- find env (\x -> fst x == from) ; 
+          return (nidx, update (fromIntegral to) arr0 env) 
+        }
 
     free s arr = 
-        let (nextidx, env) = s in 
-        Just (nextidx, filter (\(a,b,c) -> a /= arr) env)
+        let (nidx, env) = s in 
+        case find env (\(idx,_) -> idx == arr) of
+          Nothing -> Nothing
+          Just (index, (idx, arr)) -> 
+              let (head, tail)  = Data.Sequence.splitAt index env in
+              let (head' :> _)  = viewr head                      in 
+              Just (nidx, head' >< tail)
 
-    
     load s arr = copy s arr 0 
-    -}
