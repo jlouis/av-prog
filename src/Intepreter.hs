387c21adf1a4c30ed53250eb09aee5c62cd67c06
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-04-24 20:34:44 +0200

    Trailing whitespace.

--- src/Intepreter.hs
+++ src/Intepreter.hs
@@ -12,7 +12,7 @@ import Control.Applicative
 c_BIT_MASK = 0xFFFFFFFF
 
 
-interpOps s rs op_ptr = 
+interpOps s rs op_ptr =
     case index s 0 op_ptr of
       Just opc ->
           case interpOp s rs op_ptr opc of
@@ -20,37 +20,31 @@ interpOps s rs op_ptr =
             Nothing -> error "Interpretation failed"
       Nothing -> error "Could not lookup opcode"
 
-
-lookupR rs idx = 
-    do {
-      val <- index rs 0 idx ; 
+lookupR rs idx = do
+      val <- index rs 0 idx ;
       return val
-    }
 
-updateR rs idx val = 
-    update rs 0 idx val 
-
+updateR rs idx val =
+    update rs 0 idx val
 
 
-interpOpBin s rs op_ptr op1 op2 reg f = 
-    do {
-      op1'    <- lookupR rs op1   ;
-      op2'    <- lookupR rs op2   ;
-      rs'     <- updateR rs reg (f op1' op2') ; 
-      return (s, rs', op_ptr+1)
-    }
 
+interpOpBin s rs op_ptr op1 op2 reg f = do
+  op1'    <- lookupR rs op1
+  op2'    <- lookupR rs op2
+  rs'     <- updateR rs reg (f op1' op2')
+  return (s, rs', op_ptr+1)
 
-interpOp s rs op_ptr opc = 
+interpOp s rs op_ptr opc =
     let binop = interpOpBin s rs op_ptr in
-    case decode opc of 
-      Just instr -> 
-          case instr of 
+    case decode opc of
+      Just instr ->
+          case instr of
 
             Arr_Idx { ptr=ptr, offset=offset, reg=reg } ->
                 do {
                   ptr'            <- lookupR rs ptr            ;
-                  offset'         <- lookupR rs offset         ; 
+                  offset'         <- lookupR rs offset         ;
                   val'            <- index   s  ptr' offset'   ;
                   rs'             <- updateR rs reg val'     ;
                   return (s, rs', op_ptr+1)
@@ -58,21 +52,21 @@ interpOp s rs op_ptr opc =
 
             Arr_Update { ptr=ptr, offset=offset, value=value } ->
                 do {
-                  ptr'            <- lookupR   rs ptr               ; 
+                  ptr'            <- lookupR   rs ptr               ;
                   offset'         <- lookupR   rs offset            ;
-                  val'            <- lookupR   rs value           ; 
-                  s'              <- update    s  offset' ptr' val' ; 
+                  val'            <- lookupR   rs value           ;
+                  s'              <- update    s  offset' ptr' val' ;
                   return (s', rs, op_ptr+1)
                 }
 
             Move { src=src, reg=reg, guard=guard } ->
-                do { 
-                  guard'          <- lookupR   rs guard ; 
-                  case guard' of 
+                do {
+                  guard'          <- lookupR   rs guard ;
+                  case guard' of
                     0 -> return (s, rs, op_ptr+1)
                     _ -> do {
-                           src' <- lookupR rs src      ; 
-                           rs'  <- updateR rs reg src' ; 
+                           src' <- lookupR rs src      ;
+                           rs'  <- updateR rs reg src' ;
                            return (s, rs', op_ptr+1)
                          }
                 }
@@ -94,11 +88,11 @@ interpOp s rs op_ptr opc =
 
             Malloc { reg=reg, size=size } ->
                 do {
-                  (s', idx) <- allocate s size ; 
-                  rs'       <- updateR rs reg idx ; 
+                  (s', idx) <- allocate s size ;
+                  rs'       <- updateR rs reg idx ;
                   return (s', rs', op_ptr+1)
                 }
-                
+
             Free { reg=reg } ->
                 do {
                   idx    <- lookupR rs reg ;
@@ -108,7 +102,7 @@ interpOp s rs op_ptr opc =
 
             Output { value=value } ->
                 do {
-                  value' <- lookupR rs value ; 
+                  value' <- lookupR rs value ;
                   let _ = putStr [chr (fromEnum value')] in
                   return (s, rs, op_ptr+1)
                 }
@@ -122,17 +116,17 @@ interpOp s rs op_ptr opc =
 
             Load { from=from, jumppoint=jumppoint } ->
                 do {
-                  from'   <- lookupR rs from ; 
-                  s'      <- load s from'    ; 
+                  from'   <- lookupR rs from ;
+                  s'      <- load s from'    ;
                   return (s', rs, jumppoint)
                 }
 
             LoadImm { value=value, reg=reg } ->
-                do { 
-                  rs' <- updateR rs reg value ; 
+                do {
+                  rs' <- updateR rs reg value ;
                   return (s, rs', op_ptr+1)
                 }
-              
+
 
 
 
