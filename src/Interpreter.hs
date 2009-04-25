ca15cb8d05203411c62fd97bf0f8832f6f5b8174
Johan Brinch <zerrez@gmail.com>
2009-04-25 16:16:35 +0200

    bump

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -57,7 +57,7 @@ interp s rs op_ptr =
          Arr_Idx { ptr=ptr, offset=offset, reg=reg } ->
              do ptr'            <- R.getReg rs ptr
                 offset'         <- R.getReg rs offset
-                val'            <- case lookupE s  ptr' offset' of
+                val'            <- case lookupE s ptr' offset' of
                                      Just v -> return v
                                      Nothing -> error "Array out of bounds"
                 R.writeReg rs reg val'
