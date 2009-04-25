5716f635fa2373cd2bd13db0ac662d30799896fb
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-04-25 19:00:01 +0200

    Comment out tracing.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -45,7 +45,7 @@ interp s rs op_ptr =
     do instr <- case decode opcode of
                   Just instr -> return instr
                   Nothing -> error "Opcode decode failure"
-       putStrLn $ (pad ( showHex opcode "")) ++ "\t" ++ (show op_ptr) ++ "\t" ++ (show instr) 
+--       putStrLn $ (pad ( showHex opcode "")) ++ "\t" ++ (show op_ptr) ++ "\t" ++ (show instr) 
        case instr of
          Move { src=src, reg=reg, guard=guard } ->
              do guard'          <- R.getReg rs guard
