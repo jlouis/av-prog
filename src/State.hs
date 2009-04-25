d63ca9d79c1032401ba0c07a1a6ab77d8141bd05
Johan Brinch <zerrez@gmail.com>
2009-04-25 13:42:43 +0200

    COMMIT!

--- src/State.hs
+++ src/State.hs
@@ -5,26 +5,26 @@ import Data.Word
 
 class State s where
     {- init () generate new S -}
-    empty     :: [Word] -> s
+    empty     :: [Word32] -> s
 
     {- index S arr off, return val, the value stored in Array arr, Offset off -}
-    lookupE  :: s -> Word -> Word -> Maybe Word
+    lookupE  :: s -> Word32 -> Word32 -> Maybe Word32
 
     {- update S arr off val -> return S, st. index S arr off == val -}
-    updateE  :: s -> Word -> Word -> Word-> Maybe s
+    updateE  :: s -> Word32 -> Word32 -> Word32-> Maybe s
 
     {- allocate S cap, return id != 0 to Array with Capacity cap AND S where id is taken (new array contains zero) -}
-    allocate :: s -> Word -> Maybe (s, Word)
+    allocate :: s -> Word32 -> Maybe (s, Word32)
 
     {- swap S arr0 arr1, return S, st. arr0 and arr1 has been swapped -}
-    swap     :: s -> Word -> Word -> Maybe s
+    swap     :: s -> Word32 -> Word32 -> Maybe s
 
     {- copy S arr idx, return S, st. arr has been copied to idx -}
-    copy     :: s -> Word -> Word -> Maybe s
+    copy     :: s -> Word32 -> Word32 -> Maybe s
 
     {- return S where arr has been freed -}
-    free     :: s -> Word -> Maybe s
+    free     :: s -> Word32 -> Maybe s
 
     {- load S arr, return S such that Array 0 is swapped with Array arr -}
-    load     :: s -> Word -> Maybe s
+    load     :: s -> Word32 -> Maybe s
 
