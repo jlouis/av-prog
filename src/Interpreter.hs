18aa5e9bfbd8a6187a236dea72f5c353e64cc790
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-04-25 15:49:11 +0200

    Add a variant with a more direct loop in the interpreter via tailcalls.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,4 +1,4 @@
-module Interpreter (interpOps,
+module Interpreter (
                     interpret
                    )
 where
@@ -34,18 +34,7 @@ interpret' opcodes = do
 
 interpret :: [Word32] -> IO ()
 interpret opcodes = do (s, rs) <- (interpret' opcodes :: IO (WordState, R.Reg))
-                       interpOps s rs 0
-
-interpOps :: State s => s -> R.Reg -> Word32 -> IO ()
-interpOps s rs op_ptr =
-  do opcode <- case lookupE s 0 op_ptr of
-                 Just opc -> return opc
-                 Nothing -> error "Could not lookup opcode" 
-     s <- interpOp s rs op_ptr opcode -- Don't use fromIntegral here
-     case s of
-       Just (s, op_ptr) -> interpOps s rs op_ptr
-       Nothing -> return ()
-
+                       interp s rs 0
 
 lookupR :: State s => s -> Word32 -> Maybe Word32
 lookupR rs idx = lookupE rs 0 idx
@@ -56,20 +45,23 @@ updateR rs idx val = updateE rs 0 idx val
 interpOpBin :: State s =>
                s -> R.Reg -> Word32 -> Word32 -> Word32 -> Word32
                  -> (Word32 -> Word32 -> Word32)
-                 -> IO (Maybe (s, Word32))
+                 -> IO ()
 interpOpBin s rs op_ptr op1 op2 reg f = do
   op1'    <- R.getReg rs op1
   op2'    <- R.getReg rs op2
   R.writeReg rs reg (f op1' op2')
-  return $ Just (s, op_ptr+1)
-
+  interp s rs (op_ptr+1)
 
 pad orig = (take (8-(length orig)) (repeat '0')) ++ orig
 
-interpOp :: State s => s -> R.Reg -> Word32 -> Word32 -> IO (Maybe (s, Word32))
-interpOp s rs op_ptr opc =
-    let binop = interpOpBin s rs op_ptr in
-    do instr <- case decode opc of
+interp :: State s => s -> R.Reg -> Word32 -> IO ()
+interp s rs op_ptr =
+    let opcode = case lookupE s 0 op_ptr of
+                   Just opc -> opc
+                   Nothing -> error "Could not look up pos of PC"
+        binop = interpOpBin s rs op_ptr
+    in
+    do instr <- case decode opcode of
                   Just instr -> return instr
                   Nothing -> error "Opcode decode failure"
 --       putStrLn $ (show instr)  ++ "\t" ++ (pad ( showHex opc ""))
@@ -77,10 +69,10 @@ interpOp s rs op_ptr opc =
          Move { src=src, reg=reg, guard=guard } ->
              do guard'          <- R.getReg rs guard
                 case guard' of
-                  0 -> return $ Just (s, op_ptr+1)
+                  0 -> interp s rs (op_ptr+1)
                   _ -> do v <- R.getReg rs src
                           R.writeReg rs reg v
-                          return $ Just (s, op_ptr+1)
+                          interp s rs (op_ptr+1)
          Arr_Idx { ptr=ptr, offset=offset, reg=reg } ->
              do ptr'            <- R.getReg rs ptr
                 offset'         <- R.getReg rs offset
@@ -88,7 +80,7 @@ interpOp s rs op_ptr opc =
                                      Just v -> return v
                                      Nothing -> error "Array out of bounds"
                 R.writeReg rs reg val'
-                return $ Just (s, op_ptr+1)
+                interp s rs (op_ptr+1)
          Arr_Update { ptr=ptr, offset=offset, value=value } ->
              do ptr' <- R.getReg rs ptr
                 offset' <- R.getReg rs offset
@@ -96,41 +88,41 @@ interpOp s rs op_ptr opc =
                 s' <- case updateE s ptr' offset' val' of
                         Just s' -> return s'
                         Nothing -> error "Arr_Update error"
-                return $ Just (s', op_ptr+1)
+                interp s' rs (op_ptr+1)
          Add { reg=reg, op1=op1, op2=op2 } -> binop op1 op2 reg (+)
          Mul { reg=reg, op1=op1, op2=op2 } -> binop op1 op2 reg (*)
          Div { reg=reg, op1=op1, op2=op2 } -> binop op1 op2 reg div
          Nand { reg=reg, b=op1, c=op2 } -> binop op1 op2 reg (\x y -> (complement (x .&. y)))
-         Halt -> return Nothing
+         Halt -> putStrLn "*** HALT ***"
          Malloc { reg=reg, size=size } ->
              do sz <- R.getReg rs size
                 (s', idx) <- case allocate s sz of
                                Just (s', idx) -> return (s', idx)
                                Nothing -> error "Allocation failure"
                 R.writeReg rs reg idx
-                return $ Just (s', op_ptr+1)
+                interp s' rs (op_ptr+1)
          Free { reg=reg } ->
              do idx <- R.getReg rs reg
                 s' <- case free s idx of
                         Just s' -> return s'
                         Nothing -> error "Abandonment failure"
-                return $ Just (s', op_ptr+1)
+                interp s' rs (op_ptr+1)
          Output { value=value } ->
              do v <- R.getReg rs value
                 putChar (chr (fromIntegral v))
-                return $ Just (s, op_ptr+1)
+                interp s rs (op_ptr+1)
          Input { reg=reg } ->
              do c <- getChar
                 R.writeReg rs reg $ (fromIntegral . ord) c
-                return $ Just (s, op_ptr+1)
+                interp s rs (op_ptr+1)
          Load { from=from, jumppoint=jumppoint } ->
              do f <- R.getReg rs from
                 j <- R.getReg rs jumppoint
                 s' <- case load s f of
                         Just s' -> return s'
                         Nothing -> error "Couldn't load"
-                return $ Just (s', j)
+                interp s' rs j
          LoadImm { value=value, reg=reg } ->
              do R.writeReg rs reg value
-                return $ Just (s, op_ptr+1)
+                interp s rs (op_ptr+1)
 
