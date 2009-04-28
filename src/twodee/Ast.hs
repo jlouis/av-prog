7bff134beeebf868345fc4ff55c4020d9ccd66ff
Johan Brinch <zerrez@gmail.com>
2009-04-28 17:27:21 +0200

    fixes

--- src/twodee/Ast.hs
+++ src/twodee/Ast.hs
@@ -8,20 +8,22 @@ data Outface = S
              | E
 
 data Exp = Empty
-         | Tuple {exp1_e :: Exp,  exp2_e :: Exp} 
-         | Inl {exp_e :: Exp}
-         | Inr {exp_e :: Exp}
+         | Tuple { e_exp0 :: Exp,  e_exp1 :: Exp }
+         | Inl   { e_exp  :: Exp }
+         | Inr   { e_exp  :: Exp }
          | Iface Inface
 
 data Command = SendEmpty
-             | Send1 {exp_c :: Exp,  out :: Outface}
-             | Send2 {exp1_c :: Exp,  out1 :: Outface,
-                      exp2_c :: Exp,  out2 :: Outface}
-             | Case {exp_c :: Exp,  out1 :: Outface, out2 :: Outface}
-             | Split {exp_c :: Exp}
-             | Use {name :: String}
+             | Send1 { c_exp  :: Exp, out  :: Outface }
+             | Send2 { c_exp0 :: Exp, out1 :: Outface,
+                       c_exp1 :: Exp, out2 :: Outface }
+             | Case  { c_exp  :: Exp, out1 :: Outface, out2 :: Outface }
+             | Split { c_exp  :: Exp }
+             | Use   { name   :: String }
 
 
 type Wire = Maybe Integer
 
-data Box = MKBox {cmd :: Command, n :: Wire, w :: Wire, e :: Wire, s :: Wire}
+data Box = MKBox { cmd :: Command, n :: Wire, w :: Wire, e :: Wire, s :: Wire }
+
+data Module 
