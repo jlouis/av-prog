d82fd2b7c15934ef367fcc938c36429166204aeb
Johan Brinch <zerrez@gmail.com>
2009-04-28 21:32:44 +0200

    derive show

--- src/twodee/Ast.hs
+++ src/twodee/Ast.hs
@@ -1,17 +1,23 @@
 -- Abstract Syntax tree
 module Ast (Inface (..), Outface (..), Exp (..), Command (..), Wire, Box (..)) where
 
+
 data Inface = N 
             | W
+  deriving Show
 
 data Outface = S
              | E
+  deriving Show
+
 
 data Exp = Empty
          | Tuple { e_exp0 :: Exp,  e_exp1 :: Exp }
          | Inl   { e_exp  :: Exp }
          | Inr   { e_exp  :: Exp }
          | Iface Inface
+  deriving Show
+
 
 data Command = SendEmpty
              | Send1 { c_exp  :: Exp, out  :: Outface }
@@ -20,10 +26,12 @@ data Command = SendEmpty
              | Case  { c_exp  :: Exp, out1 :: Outface, out2 :: Outface }
              | Split { c_exp  :: Exp }
              | Use   { name   :: String }
+  deriving Show
 
 
 type Wire = Maybe Integer
 
-data Box = MKBox { cmd :: Command, n :: Wire, w :: Wire, e :: Wire, s :: Wire }
 
-data Module 
+data Box = MKBox { cmd :: Command, north :: Wire, west :: Wire, east :: Wire, south :: Wire }
+  deriving Show
+
