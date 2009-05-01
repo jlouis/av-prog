2e161a59a90fc0975fc93383a74d2156e09f741a
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-05-01 13:33:16 +0200

    Compilation of more trivial stuff.

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -5,6 +5,23 @@ where
 import Twodee.Ast
 import Ocult.Ast
 
+compileInt :: Int -> Exp
 compileInt 0 = Inl { e_exp = Unit }
+compileInt n | n < 0 = error "Only accepting positive integers"
+             | otherwise = Inr { e_exp = k_prime }
+                 where
+                   k_prime = compileInt (n-1)
+
+compileTerm :: Term Int -> Exp
+compileTerm (TConst k) = Inr { e_exp = n }
+    where
+      n = compileInt k
+compileTerm (TApp t1 t2) = Inl { e_exp = Tuple { e_exp0 = t1', e_exp1 = t2' }}
+    where
+      t1' = compileTerm t1
+      t2' = compileTerm t2
+
+-- Pattern compiles should make a pattern into a module.
+
 
 
