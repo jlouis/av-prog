aa23016240958debe9d860dc685d777cd4349df1
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-05-05 14:51:32 +0200

    Add a pretty-printer for Ocult.

--- src/Ocult/Ast.hs
+++ src/Ocult/Ast.hs
@@ -5,6 +5,7 @@ module Ocult.Ast(
 ) where
 
 import Data.List
+import Text.PrettyPrint
 
 -- Terms are the objects the language manipulates
 data Term a = TConst a | TApp (Term a) (Term a)
@@ -13,13 +14,56 @@ data Term a = TConst a | TApp (Term a) (Term a)
 data Pattern a b = PConst a | PVar b | PApp (Pattern a b) (Pattern a b)
 
 -- A Rule defines a pattern to *MATCH* and a *REPLACEMENT* pattern.
-data Rule = Rl (Pattern Int Int) (Pattern Int Int)
+data Rule a b = Rl (Pattern a b) (Pattern a b)
 
 -- A Program (sentence of advise) is a list of Rules
-type Program = [Rule]
+type Program a b = [Rule a b]
 
 -- Contexts define what the current variable bindings are
 type Context a = [(a, Term a)]
+
+-- Pretty-printing of Ocult
+docTerm :: Show a => Term a -> Doc
+docTerm tm =
+    case tm of
+      TConst c -> text "Const" <> (text $ show c)
+      TApp t1 t2 -> parens (t1' <> space <> t2')
+           where
+             t1' = docTerm t1
+             t2' = docTerm t2
+
+instance Show a => Show (Term a) where
+    show = render . docTerm
+
+docPattern :: (Show a, Show b) => Pattern a b -> Doc
+docPattern pat =
+    case pat of
+      PConst c -> text "Const" <> (text $ show c)
+      PVar v   -> text "Var" <> (text $ show v)
+      PApp p1 p2 -> parens (p1' <> space <> p2')
+          where
+            p1' = docPattern p1
+            p2' = docPattern p2
+
+instance (Show a, Show b) => Show (Pattern a b) where
+    show = render . docPattern
+
+docRule :: (Show a, Show b) => Rule a b -> Doc
+docRule (Rl pattern replacer) =
+    parens (pattern' <> text " => " <> replacer') <> semi
+        where
+          pattern' = docPattern pattern
+          replacer' = docPattern replacer
+
+instance (Show a, Show b) => Show (Rule a b) where
+    show = render . docRule
+
+docProgram :: (Show a, Show b) => Program a b -> Doc
+docProgram prg =
+    hcat (map docRule prg)
+
+instance (Show a, Show b) => Show (Program a b) where
+    show = render . docProgram
 
 -- Look up the term in a context
 findContext :: Eq a => Context a -> a -> Maybe (Term a)
