61e423ae94cfd2bd67177bc34183f58c906fd2d6
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-06-09 18:39:03 +0200

    Add liveness analysis to the code.

--- src/Twodee/Ast.hs
+++ src/Twodee/Ast.hs
@@ -127,11 +127,13 @@ data WireInfo = PassThrough Int
               | Start_S Int
 
 data ExplicitOrder = EOB { contents :: Command,
-                           wires :: [WireInfo] }
+                           wires :: [WireInfo],
+                           live :: [(Wire, Int)] }
 
 process_box :: Joint -> ExplicitOrder
 process_box box = EOB { contents = command box,
-                        wires = wi }
+                        wires = wi,
+                        live = [] }
     where
       wi = [End_W $ west box,
             End_N $ north box,
@@ -142,6 +144,37 @@ explicit_wiring :: [Joint] -> [ExplicitOrder]
 explicit_wiring jnts = fmap process_box ordered
   where
     ordered = topsort jnts
+
+update_liveness genset killset liveelements =
+    let
+        remove [] es = es
+        remove (e : rest) es = filter (\(w, _) -> w /= e) es
+        max_num = foldl1 max $ fmap snd liveelements
+        add n [] es = es
+        add n (e : rest) es = add (n+1) rest ((e, n+1) : es)
+    in
+      ((add max_num genset) . (remove killset)) liveelements
+
+
+liveness_analyze :: [(Wire, Int)] -> [ExplicitOrder] -> [ExplicitOrder]
+liveness_analyze l [] = []
+liveness_analyze l (b : rest) = b {live = updated_live } : (liveness_analyze updated_live rest)
+    where
+      find_gen [] = []
+      find_gen (e : es) =
+          case e of
+            Start_E i -> i : (find_gen es)
+            Start_S i -> i : (find_gen es)
+            _ -> find_gen es
+      find_kill [] = []
+      find_kill (e : es) =
+          case e of
+            End_W i -> i : (find_kill es)
+            End_N i -> i : (find_kill es)
+            _ -> find_kill es
+      gen = find_gen $ wires b
+      kill = find_kill $ wires b
+      updated_live = update_liveness gen kill l
 
 instance Show Joint where
     show b =
