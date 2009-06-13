4765ca3c5a83a34838813349bd33e4090b4c296b
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-06-14 00:13:14 +0200

    Use some Monad-fu and get rid of the environment until we need to use it.
    The implementation here avoids using a ReaderT although that could
    have been done as well. There is no need though.

--- src/SimpleCC.hs
+++ src/SimpleCC.hs
@@ -14,52 +14,54 @@ import Twodee.Ast
 
 ----------------------------------------------------------------------
 -- Name supplies for modules
-type Supply a = State (Int, [Int]) a
+type VEnvironment = [(String, Ast)]
+type Supply a = State (VEnvironment, Int, [Int]) a
 
 new :: Supply Int
 new = do
-  (count, outlist) <- get
-  put (count+1, outlist)
+  (env, count, outlist) <- get
+  put (env, count+1, outlist)
   return count
 
-type VEnvironment = [(String, Ast)]
 env_to_list :: EnvValue -> VEnvironment
 env_to_list EnvEnd = []
 env_to_list (Env (Const str ast) rest) =
     (str, ast) : (env_to_list rest)
 
-envL = lookup
+envL str = do
+  (env, _, _) <- get
+  return $ fromJust $ lookup str env
 
-compileE :: VEnvironment -> Ast -> Supply Box
-compileE _ Zero = mkBox (Send1 (Inr Unit) E)
-compileE env (Succ x) =
+compileE :: Ast -> Supply Box
+compileE Zero = mkBox (Send1 (Inr Unit) E)
+compileE (Succ x) =
     do
-      cx <- compileE env x
+      cx <- compileE x
       b <- mkBox (Send1 (Inl (Iface W)) E)
       join_ew cx b
-compileE env (Plus e1 e2) =
+compileE (Plus e1 e2) =
     do
-      c1 <- compileE env e1
-      c2 <- compileE env e2
+      c1 <- compileE e1
+      c2 <- compileE e2
       b <- (mkBox (Use "plus"))
       join c1 c2 b
-compileE env (Mul e1 e2) =
+compileE (Mul e1 e2) =
     do
-      c1 <- compileE env e1
-      c2 <- compileE env e2
+      c1 <- compileE e1
+      c2 <- compileE e2
       b <- (mkBox (Use "mul"))
       join c1 c2 b
-compileE env (Lookup s) =
+compileE (Lookup s) =
     do
-      term <- return $ fromJust $ envL s env
-      cx <- compileE env term
+      term <- envL s
+      cx <- compileE term
       b <- mkBox (Send1 (Iface W) E)
       join_ew cx b
-compileE env (Contz exp cz cs) =
+compileE (Contz exp cz cs) =
     do
-      c1 <- compileE env exp
-      ccz <- compileE env cz
-      ccs <- compileE env cs
+      c1 <- compileE exp
+      ccz <- compileE cz
+      ccs <- compileE cs
       b <- mkBox (Case (Iface W) S E)
       grp <- join_ew c1 b
       wire_e <- return $ b_east grp
@@ -68,18 +70,18 @@ compileE env (Contz exp cz cs) =
       ccs' <- return $ ccs { b_west = wire_e }
       mkBoxGrp (b_west c1) (b_north c1) (b_east ccz') (b_south ccs')
                [c1, ccz, ccs, b]
-compileE env (Call str ast) =
+compileE (Call str ast) =
     do
-      cx <- compileE env ast
+      cx <- compileE ast
       b <- mkBox (Use str)
       join_ew cx b
 
 compileF :: EnvValue -> Ast -> Mod
-compileF env ast = mkModule "main" ret
+compileF env ast = mkModule "main" ret env'
   where
     env' = env_to_list env
     ret = do
-      x <- compileE env' ast
+      x <- compileE ast
       output_e x
 
 ----------------------------------------------------------------------
@@ -102,18 +104,18 @@ output_e x =
       return x
 
 output w = do
-  (count, outs) <- get
-  put (count, w : outs)
+  (env, count, outs) <- get
+  put (env, count, w : outs)
 
-mkModule :: String -> Supply Box -> Mod
-mkModule n jnt =
+mkModule :: String -> Supply Box -> VEnvironment -> Mod
+mkModule n jnt env =
     Module { mod_boxes = [j],
              name = n,
              input_north = 0,
              input_west  = 1,
              outputs_east = outs }
   where
-    (j, (_, outs)) = (runState jnt) (2, [])
+    (j, (_, _, outs)) = (runState jnt) (env, 2, [])
 
 mkBox :: Command -> Supply Box
 mkBox c = do
